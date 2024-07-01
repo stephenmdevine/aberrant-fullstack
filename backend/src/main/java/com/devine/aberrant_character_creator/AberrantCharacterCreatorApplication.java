1ea0d402346e3e58af88fd1cc587a91fbc299693
stephenmdevine <stephenmdevine@gmail.com>
2024-06-30 22:07:32 -0500

    Fresh start

---
+++ backend/src/main/java/com/devine/aberrant_character_creator/AberrantCharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.devine.aberrant_character_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AberrantCharacterCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AberrantCharacterCreatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devine/aberrant_character_creator/AberrantCharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devine.aberrant_character_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AberrantCharacterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
